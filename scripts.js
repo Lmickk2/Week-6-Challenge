ad721840be861c5f03be91d1842b0697bbbf0bd8
Lmickk2 <lukemickan21@icloud.com>
2022-11-07 14:20:36 -0500

    units are now imperial. added wind and humidity, as well as more css styles

--- scripts.js
+++ scripts.js
@@ -1,20 +1,26 @@
 var input = document.querySelector('.input_text');
 var main = document.querySelector('#city-name');
 var temp = document.querySelector('.temp');
+var wind = document.querySelector('.wind');
+var humidity = document.querySelector('.humidity');
 var button= document.querySelector('.submit');
 
 
 button.addEventListener('click', function(name){
-fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=1ead17aedc87d3e9e1d77265edf736ed')
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&units=imperial&appid=1ead17aedc87d3e9e1d77265edf736ed')
 .then(response => response.json())
 .then(data => {
   var tempValue = data['main']['temp']
+  var windValue = data['main']['wind_speed']
+  var humidValue = data['main']['humidity']
   var nameValue = data['name'];
   var descValue = data['weather'][0]['description'];
 
   main.innerHTML = nameValue;
   temp.innerHTML = "Temp - "+tempValue;
-  input.value ="";
+  wind.innerHTML = "Wind: "+windValue;
+  humidity.innerHTML = "Humidity: "+humidValue + "%";
+  
+})
+})
 
-})
-})

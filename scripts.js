9c8e43b864f4b60816c23fcf4ec07f463f636c50
Lmickk2 <lukemickan21@icloud.com>
2022-11-07 14:59:12 -0500

    new HTML elements for daily forecast

--- scripts.js
+++ scripts.js
@@ -4,15 +4,17 @@ var temp = document.querySelector('.temp');
 var wind = document.querySelector('.wind');
 var humidity = document.querySelector('.humidity');
 var button= document.querySelector('.submit');
+var container = document.querySelector('.card')
 
 
-button.addEventListener('click', function(name){
+button.addEventListener('click', function(getWeather){
 fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&units=imperial&appid=1ead17aedc87d3e9e1d77265edf736ed')
 .then(response => response.json())
 .then(data => {
   var tempValue = data['main']['temp']
   var windValue = data['main']['wind_speed']
   var humidValue = data['main']['humidity']
+  var daily = data['main']['daily'];
   var nameValue = data['name'];
   var descValue = data['weather'][0]['description'];
 
@@ -20,7 +22,7 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&units=i
   temp.innerHTML = "Temp - "+tempValue;
   wind.innerHTML = "Wind: "+windValue;
   humidity.innerHTML = "Humidity: "+humidValue + "%";
-  
+
 })
 })
 
